5291d22212236e76eaa3a4cfc48ce3ad6fec182d
Levente Eros <levente_eros@epam.com>
2026-02-04 12:41:16 +0100

    images

--- app/blog/posts/page.tsx
+++ app/blog/posts/page.tsx
@@ -1,26 +1,18 @@
-import { posts } from '@/app/lib/placeholder-data.js';
+import { posts } from "@/app/lib/placeholder-data";
 
-export default function Component({ id, title, content, date }: { id: string, title: string, content: string, date: string }) {
-    return (
-        // <div key={id} className="border border-gray-200 p-4 my-4">
-        //     <h2>{title}</h2>
-        //     <p className="text-gray-500">{date}</p>
-        //     <p>{content}</p>
-        // </div>
-        <div className= "flex h-full flex-col px-3 py-4 md:px-2 bg-white text-black">
-
-         <ul className="list-decimal list-inside space-y-4">
-      {posts.map((post) => (
+export default function Page() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-white text-black">
+      <ul className="list-decimal list-inside space-y-4">
+        {posts.map((post) => (
           <li key={post.id}>
-          
-            title={post.title}
-            content={post.content}
-            date={post.date}
-            user={post.user}
-          
-        </li>
-      ))}
-    </ul>
-      </div>
-    );
+            <div className="font-semibold">{post.title}</div>
+            <div className="text-gray-500">{post.date}</div>
+            <div>{post.content}</div>
+            <div className="text-sm text-gray-600">{post.user}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
